9edda15a3c40363c3e815c00fc84740058d332cd
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-16 22:06:19 +0300

    test

--- models/user.py
+++ models/user.py
@@ -13,8 +13,3 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place', cascade='all, delete, delete-orphan',
-                          backref='user')
-
-    reviews = relationship('Review', cascade='all, delete, delete-orphan',
-                           backref='user')
