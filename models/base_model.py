602c2cb20268867dabb3784161078a6a0ece991c
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-16 19:41:35 +0300

    task6

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import models
 from datetime import datetime
-
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), unique=True, nullable=False,
+                             primary_key=True)
+    created_at = Column(DateTime.now(), nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DateTime.now(), nullable=False,
+                        default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         for k, v in kwargs.items():
             if not hasattr(self, k):
                 setattr(self, k, v)
         if not kwargs:
-            from models import storage
-            self.id = Column(String(60), unique=True, nullable=False,
-                             primary_key=True)
-            self.created_at = Column(datetime.now(), nullable=False,
-                                     default=datetime.utcnow())
-            self.updated_at = Column(datetime.now(), nullable=False,
-                                     default=datetime.utcnow())
+            self.id= str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.utcnow()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -45,17 +47,26 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
+        # dictionary = {}
+        # for k, v in dictionary.items():
+        #     if k is '_sa_instance_state':
+        #         del dictionary['_sa_instance_state']
+        #     dictionary.update(self.__dict__)
+        #     dictionary.update({'__class__':
+        #                        (str(type(self)).split('.')
+        #                         [-1]).split('\'')[0]})
+        #     dictionary['created_at'] = self.created_at.isoformat()
+        #     dictionary['updated_at'] = self.updated_at.isoformat()
+        #     return dictionary
         dictionary = {}
-        for k, v in dictionary.items():
-            if k is '_sa_instance_state':
-                del dictionary['_sa_instance_state']
-            dictionary.update(self.__dict__)
-            dictionary.update({'__class__':
-                               (str(type(self)).split('.')
-                                [-1]).split('\'')[0]})
-            dictionary['created_at'] = self.created_at.isoformat()
-            dictionary['updated_at'] = self.updated_at.isoformat()
-            return dictionary
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            dictionary.pop("_sa_instance_state")
+        return dictionary
 
     def delete(self):
         """to delete the current instance from the storage"""
