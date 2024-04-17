cb44a02f34da6bbdb81e9a6668e6cde2bd1b6b59
Cnnb01 <nyamambanyaguch@gmail.com>
2024-04-17 15:18:32 +0300

    amenities

--- models/user.py
+++ models/user.py
@@ -1,20 +1,57 @@
+# #!/usr/bin/python3
+# """This module defines a class User"""
+# from models.base_model import BaseModel, Base
+# from sqlalchemy import Column, String
+# from sqlalchemy.orm import relationship
+
+
+# class User(BaseModel, Base):
+#     """This class defines a user by various attributes"""
+#     __tablename__ = 'users'
+#     email = Column(String(128), nullable=False)
+#     password = Column(String(128), nullable=False)
+#     first_name = Column(String(128), nullable=True)
+#     last_name = Column(String(128), nullable=True)
+
+#     places = relationship('Place', cascade='delete',
+#                           backref='user')
+
+#     reviews = relationship('Review', cascade='delete',
+#                            backref='user')
+
 #!/usr/bin/python3
-"""This module defines a class User"""
+""" holds class User"""
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    places = relationship('Place', cascade='delete',
-                          backref='user')
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
-    reviews = relationship('Review', cascade='delete',
-                           backref='user')
+    def __setattr__(self, k, v):
+        """sets user pasword"""
+        if k == "password":
+            v = hashlib.md5(v.encode()).hexdigest()
+        super().__setattr__(k, v)
