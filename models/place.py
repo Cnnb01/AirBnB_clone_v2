18c3a937e854bb5fa11a020d346dab5c28b626f6
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-17 17:20:56 +0300

    added many-to-many relationship

--- models/place.py
+++ models/place.py
@@ -4,9 +4,15 @@ import models
 from os import getenv
 from models.base_model import BaseModel, Base
 from models.review import Review
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
+
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column("place_id", String(60), ForeignKey("places.id"), primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -24,6 +30,7 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=False)
 
     reviews = relationship("Review", backref="place", cascade="all, delete")
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=False)
 
     amenity_ids = []
 
@@ -35,3 +42,17 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     review_list.append(review)
             return review_list
+    
+        @property
+        def amenities(self):
+            """Gets amenity"""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, value):
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
