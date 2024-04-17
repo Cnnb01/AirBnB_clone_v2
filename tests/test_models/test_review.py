1921e17392b2cbc6f426a3f08adb3355b3a95992
caleb kibe <kiplagatcaleb2@gmail.com>
2024-04-17 22:47:23 +0300

    added documentation

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -5,26 +5,26 @@ from models.review import Review
 
 
 class test_review(test_basemodel):
-    """ """
+    """test case for review class"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """review test init"""
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """tests place id"""
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """tests user id"""
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """tests text"""
         new = self.value()
         self.assertEqual(type(new.text), str)
 
