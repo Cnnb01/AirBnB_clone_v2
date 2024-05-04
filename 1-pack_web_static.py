c8169bf6cf6a310921cd1e6e8841d8f993ab67ee
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-04 20:18:53 +0300

    archive file

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,14 @@ from datetime import datetime
 
 
 def do_pack():
+    """Create a tar gzipped archive of the directory web_static."""
+
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
     archive_filename = f"versions/web_static_{timestamp}.tgz"
+
     local('mkdir -p versions')
+
     created_archive = local(f"tar cf {archive_filename} web_static")
 
     if created_archive:
