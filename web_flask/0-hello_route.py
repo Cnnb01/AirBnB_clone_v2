a133cf4c4805ed74bdb3ab259e8614a01c5fd847
Cnnb01 <nyamambanyaguch@gmail.com>
2024-05-19 12:31:24 +0300

    Hello HBNB

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python3
+"""
+This script defines a simple Flask
+web application that responds with
+"Hello, HBNB!" to incoming requests.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
+    """
+    A view function that returns a simple HTML response.
+    """
     return "<p>Hello, HBNB!</p>"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
